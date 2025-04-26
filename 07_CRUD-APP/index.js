900ae691db6dd185c35b4b1d88756e0b24a3c512
Aniket Singh <aniketsingh90126@gmail.com>
2025-04-26 17:07:07 +0530

    feat: creating a new product

--- 07_CRUD-APP/index.js
+++ 07_CRUD-APP/index.js
@@ -13,18 +13,35 @@ mongoose
 
 app.set("views,", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
+// list of all products
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   res.render("products/index", { products });
 });
 
+// form to create a new product
+app.get("/products/new", (req, res) => {
+  res.render("products/new");
+});
+
+// creating new product in db
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  console.log(req.body);
+  res.redirect(`/products/${newProduct._id}`);
+});
+
+// details page for a particular product
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render("products/show", { product });
 });
 
+// listening to server on port 5000
 app.listen(5000, () => {
   console.log("listening on port 5000");
 });
