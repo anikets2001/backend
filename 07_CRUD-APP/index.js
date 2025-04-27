ef385ca7639146322ab9d6af9ce9b3714b8f71ad
Aniket Singh <aniketsingh90126@gmail.com>
2025-04-27 12:58:31 +0530

    feat: farmstand crud app done

--- 07_CRUD-APP/index.js
+++ 07_CRUD-APP/index.js
@@ -18,16 +18,26 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-const categories = ["fruits", "vegetables", "dairy"];
+const categories = ["fruit", "vegetable", "dairy"];
 
 // list of all products
 app.get("/products", async (req, res) => {
-  const products = await Product.find({});
-  res.render("products/index", { products });
+  //READ
+  const { category } = req.query;
+
+  //   if query string has a category then show only that category (filter)
+  if (category) {
+    const products = await Product.find({ category });
+    res.render("products/index", { products, category });
+  } else {
+    const products = await Product.find({});
+    res.render("products/index", { products, category: "All" });
+  }
 });
 
 // form to create a new product
 app.get("/products/new", (req, res) => {
+  //CREATE
   res.render("products/new", { categories });
 });
 
@@ -39,13 +49,14 @@ app.post("/products", async (req, res) => {
   res.redirect(`/products/${newProduct._id}`);
 });
 
-// details page for a particular product
+// details page for a particular product                (READ BY ID)
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render("products/show", { product });
 });
 
+// updating particular product                          (UPDATE)
 app.get("/products/:id/edit", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
@@ -60,6 +71,13 @@ app.put("/products/:id", async (req, res) => {
   });
 
   res.redirect(`/products/${product._id}`);
+});
+
+// delete particular product                           (DELETE)
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndDelete(id);
+  res.redirect("/products");
 });
 
 // listening to server on port 5000
